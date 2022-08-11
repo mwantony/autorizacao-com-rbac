91a58855df38e7b6efde79c635c5a8bc0a73113d
Antony Maziero Warken <againplayi7@gmail.com>
2022-08-11 15:04:59 -0300

    cap 4 vid 4 3 finalizado

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
-const InvalidArgumentError = require('./src/erros')
+const {InvalidArgumentError, NaoEncontrado, NaoAutorizado} = require('./src/erros')
 const app = require('./app')
 const port = 3000
 require('./database')
@@ -24,6 +24,12 @@ app.use((erro, req, res, proximo) => {
   if (erro instanceof jwt.TokenExpiredError) {
     status = 401
     corpo.expiradoEm = erro.expiredAt
+  }
+  if (erro instanceof jwt.NaoEncontrado) {
+    status = 404
+  }
+  if (erro instanceof jwt.NaoAutorizado) {
+    status = 401
   }
 
   res.status(status)
